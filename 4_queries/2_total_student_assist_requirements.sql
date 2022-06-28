fb8c170f5e8182a5bd71c21c5d362eb5ccde5ae5
Ricardo Moura <ricardomoura1979@gmail.com>
2022-06-28 13:27:45 +0000

    query to check the total assistance requests

--- 4_queries/2_total_student_assist_requirements.sql
+++ 4_queries/2_total_student_assist_requirements.sql
@@ -2,4 +2,4 @@ SELECT count(assistance_requests.*) as total_assistances, students.name
 FROM assistance_requests
 JOIN students ON students.id = student_id
 WHERE name = 'Elliot Dickinson'
-GROUP BY students.name;
+GROUP BY students.name; 
