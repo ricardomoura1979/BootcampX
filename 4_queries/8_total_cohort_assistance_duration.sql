5c67a9f8dbcca438f4fdf62242a090869edd053e
Ricardo Moura <ricardomoura1979@gmail.com>
2022-06-28 13:31:18 +0000

    query to find the total duration time of assistance

--- 4_queries/8_total_cohort_assistance_duration.sql
+++ 4_queries/8_total_cohort_assistance_duration.sql
@@ -3,4 +3,4 @@ FROM assistance_requests
 JOIN students ON students.id = student_id
 JOIN cohorts on cohorts.id = cohort_id
 GROUP BY cohorts.name
-ORDER BY total_duration;
+ORDER BY total_duration; 
