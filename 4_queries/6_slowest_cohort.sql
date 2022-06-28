ff75447683d16d1483f9f3d905838f0c18dbf947
Ricardo Moura <ricardomoura1979@gmail.com>
2022-06-28 13:29:57 +0000

    query to find the lowest cohort

--- 4_queries/6_slowest_cohort.sql
+++ 4_queries/6_slowest_cohort.sql
@@ -4,4 +4,4 @@ JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = cohort_id
 GROUP BY cohorts.name
 ORDER BY average_assistance_time DESC
-LIMIT 1;
+LIMIT 1; 
