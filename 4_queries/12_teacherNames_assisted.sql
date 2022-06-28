97175e7cb286184436332772b99ce2ff02514074
Ricardo Moura <ricardomoura1979@gmail.com>
2022-06-28 13:34:18 +0000

    query to seek all teacher's name who provided assistance during a cohort

--- 4_queries/12_teacherNames_assisted.sql
+++ 4_queries/12_teacherNames_assisted.sql
@@ -4,4 +4,4 @@ JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
-ORDER BY teacher;
+ORDER BY teacher; 
